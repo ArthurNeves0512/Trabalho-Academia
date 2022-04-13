668f3a38723f5d0d9734001ec7bb1272cac9b0d5
Arthur Neves <82537546+ArthurNeves0512@users.noreply.github.com>
2022-04-13 16:55:49 -0300

    Add files via upload
    PRONTO AGR É ESSA

---
+++ Academia/src/classes/Pessoa.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author arthur
+ */
+public class Pessoa {
+    protected String nome;
+    protected String senha;
+    protected String cpf;
+    
+    public Pessoa(){
+        
+    }
+    
+    public Pessoa(String nome, String senha, String cpf){
+        setSenha(senha);setNome(nome);setCpf(cpf);
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+}
